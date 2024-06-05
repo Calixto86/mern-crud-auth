6c1fc7ffd1d71fe6dda59493b25c5e642989fddb
Calixto86 <calixtomamani972@gmail.com>
2024-06-05 01:25:49 -0400

    crud de tareas guardando las tareas con user al que pertenecen

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ modulos:
 npm i bcryptjs: para encriptar la contracenia
 npm i jsonwebtoken: para identificar al usuario registrado
 npm i cookie-parser: es como un expressjson para cookies que convierte la cookies en un objeto json
+
+404: error de no encontrado
+204: todo estuvo bien pero no hay respuesta
--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js'
+import taskRoutes from './routes/tasks.routes.js'
 
 const app = express();
 
-app.use(morgan('dev'));
-
+app.use(morgan('dev'));  
 app.use(express.json());//convertir los requiest body en formato json
 app.use(cookieParser());//convertir los requiest body en formato json para cookies
 
 app.use("/api", authRoutes);
+app.use("/api", taskRoutes);
 
 
 export default app;
