6e1e3ed90a76202043db571d7ff37369762ec433
Calixto86 <calixtomamani972@gmail.com>
2024-07-23 19:00:30 -0400

    validacion de errores, contexto de usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,6 +6,10 @@ export const register = async (req, res) => {
     const { email, password, username } = req.body;
 
     try {
+
+        const userFound = await User.findOne({email})
+        if (userFound) return res.status(400).json(["the email is already in use" ]);
+
         const passwordHash = await bcrypt.hash(password, 10)//se ejecuta 10 veces el algoritmo
 
         const newUser = new User({
